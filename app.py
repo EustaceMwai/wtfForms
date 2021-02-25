2784c078e85e1f5db09de16a7c1e507c923a90b5
EustaceMwai <43333927+EustaceMwai@users.noreply.github.com>
2021-02-25 21:49:24 +0300

    goggle recaptcha

--- app.py
+++ app.py
@@ -1,41 +1,30 @@
 from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, IntegerField, BooleanField
-from wtforms.validators import InputRequired, Length, AnyOf, Email
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, AnyOf
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Mysecret!'
+app.config['SECRET_KEY'] = 'Thisisasecret!'
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LdCy2caAAAAAKiqFiG4IWx1JmIR45EKJUxVbLFP'
+app.config['RECAPTCHA_PRIVATE_KEY'] = 'Y6LdCy2caAAAAABulZ4-AcKQ6fz5sfuH99p6Dsmxk'
+app.config['TESTING'] = True
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Your Username', validators=[InputRequired('A username is required!'), Length(min=4, max=8,
-                                                                                                         message='Must be between 4 and 8 characters')])
+    username = StringField('username', validators=[InputRequired('A username is required!'), Length(min=5, max=10,
+                                                                                                    message='Must be between 5 and 10 characters.')])
     password = PasswordField('password',
-                             validators=[InputRequired('Password is required!'), AnyOf(values=['secret', 'password'])])
-    age = IntegerField('age', default=24)
-    true = BooleanField('Click here')
-    email = StringField('email', validators=[Email()])
+                             validators=[InputRequired('Password is required!'), AnyOf(values=['password', 'secret'])])
+    recaptcha = RecaptchaField()
 
 
-class User:
-    def __init__(self, username, age, email):
-        self.username = username
-        self.age = age
-        self.email = email
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    myuser = User('Eustero', 23, 'eustero@gmail.com')
-
-    form = LoginForm(obj=myuser)
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    form = LoginForm()
 
     if form.validate_on_submit():
-        return '<h1>Username: {} Password: {} Age: {} True: {} Email: {}</h1>'.format(form.username.data,
-                                                                                      form.password.data, form.age.data,
-                                                                                      form.true.data, form.email.data)
-
-    return render_template('index.html', form=form)
+        return '<h1>The username is {}. The password is {}.'.format(form.username.data, form.password.data)
+    return render_template('form.html', form=form)
 
 
 if __name__ == '__main__':
